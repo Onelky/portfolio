be7971ee49943616a6b5e9e9d397ab443a83d51f
onelky <onelky.hernandez29@gmail.com>
2022-04-17 16:43:26 -0400

    Add name component

--- src/__tests__/components/CoverName.spec.tsx
+++ src/__tests__/components/CoverName.spec.tsx
@@ -2,14 +2,14 @@ import { render, screen } from '@testing-library/react'
 import CoverName from '../../components/CoverName/CoverName'
 import '@testing-library/jest-dom/extend-expect';
 
-// test('Renders coverName component', () => {
-//         render(<CoverName />)
-//         const name = screen.getByRole('name');
-//         expect(name).toBeInTheDocument();
-// });
-//
-// test('Renders three arrow elements', () => {
-//     render(<CoverName />)
-//     const name = screen.getByRole('name');
-//     expect(name).toHaveTextContent('Onelky');
-// })
+test('Renders coverName component', () => {
+        render(<CoverName />)
+        const component = screen.getByRole('coverName');
+        expect(component).toBeInTheDocument();
+});
+
+test('Contains name', () => {
+    render(<CoverName />)
+    const component = screen.getByRole('coverName');
+    expect(component).toHaveTextContent('Onelky');
+})
