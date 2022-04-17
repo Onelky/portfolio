d19164aa5d899e5cf6795c01cc7e272f2f51f2a8
onelky <onelky.hernandez29@gmail.com>
2022-04-17 17:55:19 -0400

    Create header section

--- src/pageComponents/Header/Header.tsx
+++ src/pageComponents/Header/Header.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import CoverName from "../../components/CoverName/CoverName";
 import Arrows from "../../components/Arrows/Arrows";
+import Grid from "@mui/material/Grid";
 
-// props: { size: string } ?
-
-/* Initial presentation. */
+/* Page's first section that contains creator's name. */
 const Header = (): JSX.Element => {
     return (
-        <div role={"header"}>
-            <CoverName/>
-            <Arrows/>
-        </div>
+        <Grid container role="header"
+              height="100vh"
+              flexWrap={'nowrap'}
+              direction={'column'}
+              alignItems="center">
+
+            <Grid item height={'100%'} display={'flex'} alignItems="center" xs={10} role={'coverName'}>
+                <CoverName/>
+            </Grid>
+
+            <Grid item height={'100%'} xs={2} justifySelf={'flex-end'} role={'arrows'}>
+                <Arrows/>
+            </Grid>
+
+        </Grid>
     );
 };
 
