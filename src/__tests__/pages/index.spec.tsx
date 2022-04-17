2ae284cb964ab281885670fe5a64e1d7b0fe9d20
onelky <onelky.hernandez29@gmail.com>
2022-04-17 16:38:10 -0400

    Add global styles

--- src/__tests__/pages/index.spec.tsx
+++ src/__tests__/pages/index.spec.tsx
@@ -1,11 +1,9 @@
 import { render, screen } from '@testing-library/react'
-import Header from '../../pageComponents/Header/Header'
+import App from '../../pages/index'
 import '@testing-library/jest-dom/extend-expect';
-import Arrows from "../../components/Arrows/Arrows";
-import {name} from '../../constants/header';
 
 test('Renders header section', () => {
-        render(<Header />)
+        render(<App />)
         const headerComponent = screen.getByRole('header');
         expect(headerComponent).toBeInTheDocument();
 });
