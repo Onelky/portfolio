c90c2195c0e980df4a3328022efcc7ca35e8017a
onelky <onelky.hernandez29@gmail.com>
2022-05-20 22:35:59 -0400

    Update icon component and add some svg icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,15 +1,26 @@
 import * as React from "react"
 import {SvgIcon} from "@mui/material";
+import {email, github, linkedIn} from "../../constants/iconNames";
+import GithubSvg from '../../../public/icons/github.svg'
+import LinkedInSvg from '../../../public/icons/linkedIn.svg'
+import EmailSvg from '../../../public/icons/email.svg'
 
-const Icon = ({svg: SvgImg, ...props}) => (
+const getImage = (iconName) => {
+    if (iconName === github) return <GithubSvg/>;
+    if (iconName === linkedIn) return <LinkedInSvg/>;
+    if (iconName === email) return <EmailSvg/>;
+}
+
+const Icon = ({name, hoverColor = false, ...props}) => (
     <SvgIcon sx={{
         fontSize: 26,
-        ':hover': {
+        ...(hoverColor && {
+            ':hover': {
             color: 'var(--accent-color)',
             cursor: 'pointer'
-        }
+        }})
     }}{...props}>
-        <SvgImg/>
+        {getImage(name)}
     </SvgIcon>
 
 )
---
+++ src/constants/iconNames.ts
@@ -0,0 +1,3 @@
+export const github: string = 'github';
+export const linkedIn: string = 'linkedIn';
+export const email: string = 'email';
