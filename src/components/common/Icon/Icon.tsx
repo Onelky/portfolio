ee6c839053d327fb7d3edf67654c28ff6eb51659
onelky <onelky.hernandez29@gmail.com>
2022-05-29 00:48:32 -0400

    Remove getImage function from Icon component and update h4 typography size

--- src/components/common/Icon/Icon.tsx
+++ src/components/common/Icon/Icon.tsx
@@ -1,28 +1,22 @@
 import * as React from "react"
 import {SvgIcon} from "@mui/material";
-import {email, github, linkedIn} from "../../../constants/iconNames";
-import GithubSvg from '../../../../public/icons/github.svg'
-import LinkedInSvg from '../../../../public/icons/linkedIn.svg'
-import EmailSvg from '../../../../public/icons/email.svg'
+import getIconImage from "../../../lib/utils";
 
-const getImage = (iconName) => {
-    if (iconName === github) return <GithubSvg/>;
-    if (iconName === linkedIn) return <LinkedInSvg/>;
-    if (iconName === email) return <EmailSvg/>;
-}
 
-const Icon = ({name, hoverColor = false, ...props}) => (
-    <SvgIcon sx={{
+const Icon = ({name, hoverColor = false, ...props}) => {
+    const Icon = getIconImage(name);
+    return <SvgIcon sx={{
         fontSize: 26,
         ...(hoverColor && {
             ':hover': {
-            color: 'var(--accent-color)',
-            cursor: 'pointer'
-        }})
+                color: 'var(--accent-color)',
+                cursor: 'pointer'
+            }
+        })
     }}{...props}>
-        {getImage(name)}
+        <Icon/>
     </SvgIcon>
 
-)
+}
 
 export default Icon
