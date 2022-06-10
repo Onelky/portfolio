095390053339d20be9143be6009b1df9748d66e8
onelky <onelky.hernandez29@gmail.com>
2022-06-10 19:44:09 -0400

    Create ToolIcon component

---
+++ src/components/common/ToolIcon/ToolIcon.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent } from 'react';
+import Icon from "../Icon/Icon";
+import {Typography} from "@mui/material";
+import Stack from "@mui/material/Stack";
+
+interface Props {
+    name: string,
+    horizontalLayout?: boolean,
+    iconSize?: number,
+}
+
+const ToolIcon: FunctionComponent<Props> = (props) => {
+    const { name = '',  iconSize = 30, horizontalLayout = false} = props;
+  return (
+      <Stack direction={horizontalLayout ? 'row' : 'column'}
+             p={'5px'}
+             height={60}
+             alignItems={'center'}
+             justifyContent={'center'}
+             sx={{
+                 ':hover': {
+                     transform: 'translateY(-5px)'
+                 },
+                 transition: 'all 400ms ease-in-out'
+             }}>
+          <Icon name={name} size={iconSize}></Icon>
+          <Typography variant={'subtitle1'} sx={{color: 'var(--accent-color) !important'}}>{name}</Typography>
+
+      </Stack>
+  );
+};
+
+export default ToolIcon;
