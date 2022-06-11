247b9ac118f05ca7eff360fd80d7e9e85cdd3964
onelky <onelky.hernandez29@gmail.com>
2022-06-10 22:21:16 -0400

    Update ContactSideBar to use getIconPath util and update icons colors

--- src/components/common/ContactSideBar/ContactSideBar.tsx
+++ src/components/common/ContactSideBar/ContactSideBar.tsx
@@ -5,18 +5,19 @@ import Line from "../Line/Line";
 import Stack from "@mui/material/Stack";
 import Icon from "../Icon/Icon";
 import {email, github, linkedIn} from "../../../constants/iconNames";
+import getIconPath from "../../../lib/utils";
 
 const IconsContainer = ({isMobile}) => {
     return (
         <Stack direction={isMobile ? "row" : "column"} spacing={1}>
             <a target="_blank" style={{height: '26px'}} href={'https://github.com/Onelky/'}>
-                <Icon name={github} hoverColor/>
+                <Icon name={getIconPath(github)} useHoverEffect/>
             </a>
             <a target="_blank" style={{height: '26px'}} href={'https://www.linkedin.com/in/onelky/'}>
-                <Icon name={linkedIn} hoverColor/>
+                <Icon name={getIconPath(linkedIn)} useHoverEffect/>
             </a>
             <a target="_blank" href="mailto:onelky.hernandez29@gmail.com" style={{height: '26px'}}>
-                <Icon name={email} hoverColor/>
+                <Icon name={getIconPath(email)} useHoverEffect/>
             </a>
         </Stack>
     );
