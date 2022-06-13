4d30a88f30ba58d3689107fbe58ca58d4507e78b
onelky <onelky.hernandez29@gmail.com>
2022-06-13 19:50:12 -0400

    Create TechnologiesList component

--- src/pageComponents/Projects/Projects.tsx
+++ src/pageComponents/Projects/Projects.tsx
@@ -5,6 +5,9 @@ import linePositions from "../../constants/sectionHeader";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import getIconPath from "../../lib/utils";
+import ToolIcon from "../../components/common/ToolIcon/ToolIcon";
+import {styled} from "@mui/material/styles";
 
 // todo: Add constant file with all this
 
@@ -20,12 +23,42 @@ interface Props {
 // todo: Add images of projects, add effects
 // todo: Add github and demo links
 // todo: Add description
-// todo: Customize the toolIcon, have a different background
 
+const TechnologyPill = styled(Grid)(() => ({
+    background: 'rgba(2, 2, 4, 0.61)',
+    borderRadius: 12,
+    maxWidth: 'max-content !important',
+    padding: '0 5px',
+    ':hover': {
+        transform: 'translateY(-3px)'
+    },
+    transition: 'all 400ms ease-in-out'
+}))
+
+const TechnologiesList = ({technologies}: {technologies: string[]}) => {
+    return (
+        <Grid container
+              justifyContent={'center'}
+              maxWidth={350}
+              m={'10px auto'}
+              rowGap={1}
+              columnGap={1}
+        >
+            {
+                technologies.map((name) => (
+                    <TechnologyPill item key={getIconPath(name)} xs={3} md={4}>
+                        <ToolIcon horizontalLayout label={name} iconSize={18} name={getIconPath(name)} />
+                    </TechnologyPill>
+                ))
+            }
+
+        </Grid>
+    );
+};
 
 
 const Project: FunctionComponent<Props> = (props) => {
-    const {projectName} = props
+    const {projectName, technologies} = props
 
     return (
         <Grid container justifyContent={'space-around'}>
@@ -39,7 +72,7 @@ const Project: FunctionComponent<Props> = (props) => {
                             fontWeight={400}>
                     {projectName}
                 </Typography>
-                Description
+                <TechnologiesList technologies={technologies}/>
 
             </Grid>
         </Grid>
@@ -58,7 +91,7 @@ const Projects: FunctionComponent = () => {
             <Project projectName={'MyTasks'}
                      picture={'#'}
                      description={'Lorem ipsun'}
-                     technologies={['Angular']}
+                     technologies={['Angular', 'TypeScript', 'Bootstrap', 'C Sharp', 'SQLite', 'Docker']}
                      demo={'#'}
                      github={'#'}
             />
