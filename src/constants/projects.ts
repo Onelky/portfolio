7dde013c3bd9422534f6069f2e8de234f6db4b35
onelky <onelky.hernandez29@gmail.com>
2022-06-17 21:45:03 -0400

    Create Projects section

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,19 +1,38 @@
+import * as tools from "./iconNames";
+
 export interface Project {
+    id: string,
     projectName: string,
-    picture: string,
+    imagePath: string,
     technologies: string[],
     description: string,
     demoUrl: string,
     githubUrl: string
 }
 
-export const project1 = {
+export const myTasks: Project = {
+    id: 'myTasks',
     projectName: 'MyTasks',
-    picture: '#',
-    technologies: ['Angular', 'TypeScript', 'Bootstrap', 'C Sharp', 'SQLite', 'Docker'],
+    imagePath: 'mytasks',
+    technologies: [tools.angular, tools.typescript, tools.bootstrap, tools.cSharp, tools.sqLite, tools.docker],
     description: 'Web application that facilitates the management of pendant tasks and their classification.'  +
-        '    This app includes a built-in pomodoro timer makes easier to track the time spent on each task and' +
+        '    It includes a built-in pomodoro timer that makes easier to track the time spent on each task, alongside' +
         '    different ways of sorting/filtering created tasks.',
     demoUrl: 'https://mytasks-xi.vercel.app',
     githubUrl: 'https://github.com/Onelky/mytasksFront'
 }
+export const falcon: Project = {
+    id: 'falcon',
+    projectName: 'Falcon',
+    imagePath: 'falcon',
+    technologies: [tools.react, tools.javascript, tools.redux, tools.cSharp, tools.sqlServer],
+    description: 'A web application that can be used to create, edit and control the organizational structure of a given institution. ' +
+        'It also allows to identify and manage potential risks that could threat an organization, as well to apply controls for the prevention of said risks.' +
+        ' Includes authentication and authorization features by using JWT tokens, so users can only visualize contents by their role. ',
+    demoUrl: 'https://falcon-frontend.vercel.app',
+    githubUrl: 'https://github.com/IDS33X/falcon-frontend'
+}
+export const projects: Project[] = [
+    myTasks,
+    falcon
+]
--- src/pageComponents/Projects/Projects.tsx
+++ src/pageComponents/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, {FunctionComponent} from 'react';
 import Stack from "@mui/material/Stack";
 import SectionHeader from "../../components/common/SectionHeader/SectionHeader";
 import linePositions from "../../constants/sectionHeader";
@@ -6,28 +6,50 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TechnologiesList from "../../components/Projects/TechnologiesList";
-import {Project, project1} from "../../constants/projects";
+import {Project, projects} from "../../constants/projects";
 import Icon from "../../components/common/Icon/Icon";
 import getIconPath from "../../lib/utils";
 import {demo, github} from "../../constants/iconNames";
-
-// todo: Add constant file with all this
-
-// todo: Add responsive image and text above image
-// todo: Add images of projects, add effects
+import Image from "next/image";
 
 export interface Props {
     project: Project,
     containerDirection?: string,
 }
+// todo: Add responsive design to image and text above image
 
-const Project = ({project, containerDirection} : Props ) => {
-    const {projectName, technologies, description, githubUrl, demoUrl} = project;
+const Project = ({project, containerDirection = 'row'} : Props ) => {
+    const {projectName, technologies, imagePath, description, githubUrl, demoUrl} = project;
 
     return (
-        <Grid container justifyContent={'space-around'} direction={containerDirection || 'row'}>
-            <Grid item xs={12} md={6} sx={{backgroundColor: 'green'}}>
-                Picture
+        <Grid container
+              justifyContent={'space-around'}
+              flexDirection={containerDirection}>
+            <Grid item
+                  xs={12}
+                  md={6}
+                  width={560}
+                  height={310}
+                  position={'relative'}
+                  sx={{
+                      '& span::before': {
+                          zIndex: 1,
+                          position: 'absolute',
+                          display: 'block',
+                          width: '100%',
+                          height: '100%',
+                          background: 'rgba(88, 181, 169, 0.4)',
+                          content: '""',
+                          cursor: 'pointer',
+                      },
+                      '& :hover::before': {
+                          background: 'rgba(59,70,121,0.4)',
+                      }
+                  }}
+            >
+                <a target="_blank" href={demoUrl}>
+                    <Image src={ `/images/${imagePath}.png`} layout={'fill'}></Image>
+                </a>
             </Grid>
             <Grid item xs={12} md={6} p={'0 70px'}>
                 <Typography variant={'h3'}
@@ -54,18 +76,16 @@ const Project = ({project, containerDirection} : Props ) => {
     );
 };
 const Projects: FunctionComponent = () => {
-
-    // todo: Add content component
-    // todo: Work on the structure of projects
-
     return (
-        <Stack justifyContent={'space-around'} mt={15} spacing={8}>
+        <Stack justifyContent={'space-around'} mt={15} spacing={8} component={'section'}>
             <Box sx={{width: {xs: '100%', md: '50%'}}}>
                 <SectionHeader title={'Projects'} linePosition={linePositions.right}/>
             </Box>
-            <Project project={project1} />
-            <Project project={project1} containerDirection={'row-reverse'}/>
-
+            {
+                projects.map((project, index) => (
+                    <Project key={project.id} project={project} containerDirection={(index % 2 !== 0) &&  'row-reverse'} />
+                ))
+            }
         </Stack>
     );
 };
