634b27419b92bc935a82d03177f614d385d7db7d
onelky <onelky.hernandez29@gmail.com>
2022-07-03 03:56:13 -0400

    Update Props in BlueButton components

--- src/components/common/BlueButton/BlueButton.tsx
+++ src/components/common/BlueButton/BlueButton.tsx
@@ -50,10 +50,10 @@ interface Props {
     onClick?: any,
     type?: 'submit' | 'reset' | 'button'
 }
-const BlueButton = ({text, onClick, type, ...props}: Props) => {
+const BlueButton = ({text, onClick, ...props}: Props) => {
 
     return (
-        <AnimatedContainer onClick={onClick} type={type || 'button'} {...props}>
+        <AnimatedContainer onClick={onClick} {...props}>
             <svg>
                 <rect x="0" y="0" fill="none" width="100%" height="100%"/>
             </svg>
